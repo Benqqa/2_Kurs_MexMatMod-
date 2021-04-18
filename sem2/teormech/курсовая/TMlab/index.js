f82a2b40c70375b09a802a1e43c9ba7716fed2dc
Benqqa <53792741+Benqqa@users.noreply.github.com>
2021-04-18 19:17:12 +0300

    start work

--- sem2/teormech/курсовая/TMlab/index.js
+++ sem2/teormech/курсовая/TMlab/index.js
@@ -1,13 +1,15 @@
-var canvas = document.getElementById('stage');
+var canvas;
 var ctx = canvas.getContext("2d");
 ctx.lineWidth= 2;
  var position = 0;
- setInterval(function (){
+ setInterval(Draw(), 20);
+ function Draw(){
+   canvas = document.getElementById('stage');
+   ctx = canvas.getContext("2d");
    ctx.clearRect(0,0,400,400);
    ctx.fillRect(position,0,20,20);
    position++
    if(position> 200){
       position=0;
    }
-
- }, 20);
+ }
